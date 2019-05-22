ad71aa43dd7c12b628438524d2892bc7448b6a88
Unknown <hdudowicz@gmail.com>
2019-05-22 19:34:04 +0100

    tests

--- D4/org.eclipse.papyrus.javagen.BusDisplay/src/BusDisplay/BusStopDisplayTest.java
+++ D4/org.eclipse.papyrus.javagen.BusDisplay/src/BusDisplay/BusStopDisplayTest.java
@@ -1,120 +1,37 @@
 package BusDisplay;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayOutputStream;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.lang.String;
 import java.time.LocalTime;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
+
 
 public class BusStopDisplayTest {
-    //Defining byte array output stream to test for console printed messages
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
 
     @Test
-    public void createWithBadPaths() {
-        assertThrows(IOException.class,
-                () -> new BusStopDisplay().create("bad.path", "bad.path"));
-    }
-
-    @Test
-    void createWithCorrectPaths()  {
-        BusStopDisplay display = null;
-        try {
-            display = new BusStopDisplay().create("..\\stop_info.csv", "..\\routes.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertNotNull(display);
-    }
-
-    //Setting output stream to variable made earlier to access messages printed to console
-    @BeforeEach
-    void setUp() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(originalOut);
-    }
-
-    @Test
-    public void addScheduledToExpected() throws IOException {
+    public void addToExpectedTest() throws IOException {
         BusStopDisplay display = new BusStopDisplay();
-
-
         display.routeList = RoutesAndStopInfoParser.parseRouteList("../routes.csv");
-        assertNotNull(display.routeList);
-
-        assertNull(display.expectedBusList);
-
         display.addScheduledToExpected();
+
         assertFalse(display.expectedBusList.isEmpty());
+    }
 
+    @Test(expected = FileNotFoundException.class)
+    public void emptyPathTest() throws IOException {
+        BusStopDisplay busStopDisplay = new BusStopDisplay().create("", "");
     }
 
     @Test
-    public void getCallingRoutes() {
-        BusStopDisplay display = null;
-        try {
-            display = new BusStopDisplay().create("..\\stop_info.csv","..\\routes.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(display.getCallingRoutes(), display.routeList);
-    }
-
-    @Test
-    public void getDepartureTimes() {
-        BusStopDisplay display = null;
-        try {
-            display = new BusStopDisplay().create("..\\stop_info.csv","..\\routes.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] route3Departures = {"08:11", "09:11", "10:11", "11:11", "12:11",
-                "13:11", "14:11", "15:11", "16:11",
-                "17:11", "18:11", "19:11", "20:11", "21:11"};
-        String[] actualTimes = new String[14];
-        for (int i = 0; i < display.getDepartureTimes(3).size(); i++){
-            actualTimes[i] = display.getDepartureTimes(3).get(i).toString();
-        }
-
-        assertArrayEquals(actualTimes, route3Departures);
-    }
-
-    @Test
-    public void getTimeOfNextBus() {
-        BusStopDisplay display = null;
-        try {
-            display = new BusStopDisplay().create("..\\stop_info.csv","..\\routes.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(display.getTimeOfNextBus(3, LocalTime.of(6, 0)), LocalTime.of(8, 11));
-    }
-
-    @Test
-    public void testRemovePassedBus() {
-        BusStopDisplay display = null;
-        try {
-            display = new BusStopDisplay().create("..\\stop_info.csv","..\\routes.csv");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ExpectedBus firstBus = display.expectedBusList.get(0);
-        assertTrue(display.expectedBusList.contains(firstBus));
-        display.disp(LocalTime.of(7, 10));
-        assertFalse(display.expectedBusList.contains(firstBus));
+    public void rightPathTest() throws IOException {
+        BusStopDisplay busStopDisplay = new BusStopDisplay().create("..\\stop_info.csv", "..\\routes.csv");
+        assertNotEquals(busStopDisplay, null);
     }
 }
--- D4/org.eclipse.papyrus.javagen.BusDisplay/src/BusDisplay/RoutesAndStopInfoParserTest.java
+++ D4/org.eclipse.papyrus.javagen.BusDisplay/src/BusDisplay/RoutesAndStopInfoParserTest.java
@@ -1,7 +1,7 @@
 package BusDisplay;
 
-import javafx.util.Pair;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,12 +10,44 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 public class RoutesAndStopInfoParserTest extends RoutesAndStopInfoParser {
-    
+
     @Test
-    void parseRouteTest() {
+    void pasreRoutes() {
+        try {
+            ArrayList<LocalTime> testTimeList = new ArrayList<>();
+            testTimeList.add(LocalTime.parse("08:11"));
+            testTimeList.add(LocalTime.parse("09:11"));
+            testTimeList.add(LocalTime.parse("10:11"));
+            testTimeList.add(LocalTime.parse("11:11"));
+            testTimeList.add(LocalTime.parse("12:11"));
+            testTimeList.add(LocalTime.parse("13:11"));
+            testTimeList.add(LocalTime.parse("14:11"));
+            testTimeList.add(LocalTime.parse("15:11"));
+            testTimeList.add(LocalTime.parse("16:11"));
+            testTimeList.add(LocalTime.parse("17:11"));
+            testTimeList.add(LocalTime.parse("18:11"));
+            testTimeList.add(LocalTime.parse("19:11"));
+            testTimeList.add(LocalTime.parse("20:11"));
+            testTimeList.add(LocalTime.parse("21:11"));
+
+            ArrayList<Route> testRoutesParsed = RoutesAndStopInfoParser.parseRouteList("../routes_test.csv");
+            ArrayList<Route> testRoutes = new ArrayList<>(Arrays.asList(new Route(3,
+                    "Vila Nova", "Centennial park", testTimeList)));
+
+            assertEquals(testRoutesParsed.get(0).routeNo, testRoutes.get(0).routeNo);
+            assertEquals(testRoutesParsed.get(0).origin, testRoutes.get(0).origin);
+            assertEquals(testRoutesParsed.get(0).destination, testRoutes.get(0).destination);
+            assertEquals(testRoutesParsed.get(0).schedule, testRoutes.get(0).schedule);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void parseOneRoute() {
         String testRouteConfig = "3,Vila Nova,Centennial park";
 
         ArrayList<LocalTime> testTimeList = new ArrayList<>();
@@ -41,52 +73,13 @@ public class RoutesAndStopInfoParserTest extends RoutesAndStopInfoParser {
         try {
             parsedRoute = RoutesAndStopInfoParser.parseRoute(testRouteConfig);
         } catch (IOException e) {
-            fail();
-        }
-
-        assertEquals();
-        assertEquals(parsedRoute.schedule, testRoute.schedule);
-    }
-
-    @Test
-    void pasreRoutesTest() {
-        try {
-            ArrayList<Route> testRoutesParsed = RoutesAndStopInfoParser.parseRouteList("../routes_test.csv");
-            ArrayList<Route> testRoutes = new ArrayList<>(Arrays.asList(new Route(3,
-                    "Vila Nova", "Centennial park",
-                    new ArrayList<LocalTime>(Arrays.asList(LocalTime.of(8, 11),
-                            LocalTime.of(9, 11),
-                            LocalTime.of(10, 11),
-                            LocalTime.of(11, 11),
-                            LocalTime.of(12, 11))))));
-
-            assertTrue(testRoutesParsed.equals(testRoutes));
-        } catch (IOException e) {
             e.printStackTrace();
         }
 
+        assertEquals(parsedRoute.routeNo, testRoute.routeNo);
+        assertEquals(parsedRoute.origin, testRoute.origin);
+        assertEquals(parsedRoute.destination, testRoute.destination);
+        assertEquals(parsedRoute.schedule, testRoute.schedule);
     }
 
-    @Test
-    void parseBusInfo() {
-        Pair<String, String> testStopInfo;
-        try{
-            BufferedReader file = new BufferedReader(
-                    new FileReader("../stop_info.csv"));
-
-            String line = file.readLine();
-
-            if(line == null) {
-                file.close();
-            } else {
-                String[] parts = line.split(",");
-                file.close();
-                testStopInfo = new Pair<>(parts[0], parts[1]);
-
-                assertEquals(testStopInfo, new Pair<>("BS05", "Sweetspot"));
-            }
-        } catch (IOException e){
-            fail();
-        }
-    }
 }
